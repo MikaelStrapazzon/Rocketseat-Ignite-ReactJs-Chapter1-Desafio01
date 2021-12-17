d32bc8333bf8c220ba877d1a0e92da445bd458d0
MikaelStrapazzon <mikael.quadros@gmail.com>
2021-12-17 17:33:00 -0300

    Desenvolvimento do desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,33 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if( newTaskTitle ) {
+      setTasks([
+        ...tasks,
+        {
+          id: Math.random(),
+          title: newTaskTitle,
+          isComplete: false
+        }
+      ])
+
+      setNewTaskTitle("");
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let tasksUpdate = tasks.slice();
+    let index = tasksUpdate.findIndex( task => task.id == id );
+
+    tasksUpdate[index].isComplete = !tasksUpdate[index].isComplete;
+
+    setTasks( tasksUpdate );
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    let tasksUpdate = tasks.slice();
+
+    setTasks( tasksUpdate.filter( task => task.id != id ) );
   }
 
   return (
